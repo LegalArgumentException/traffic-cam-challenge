dd154b8b362e5e45b283b11ebecfe5b17d1bb2a4
Tanner Garrett <tanner@garrett.org>
2014-12-11 02:40:37 -0800

    First and final commit

--- js/app.js
+++ js/app.js
@@ -11,3 +11,57 @@
 //you should also write the code to filter the set of markers when the user
 //types a search phrase into the search box
 
+$(document).ready(function () {
+	function initialize() {
+        var mapOptions = {
+          center: { lat: 47.6, lng: -122.3},
+          zoom: 12
+        };
+        var map = new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+        google.maps.event.addListener(map, "click", function() {
+        	infoWindow.close();
+        });
+        $.getJSON("http://data.seattle.gov/resource/65fc-btcc.json", function(data) {
+    		$.each(data, function(i, item) {
+    			var markerLatLng = new google.maps.LatLng(item.location.latitude, item.location.longitude);
+    			var marker = new google.maps.Marker({
+    				position : markerLatLng,
+    				map: map,
+    				cameraLabel: item.cameralabel
+    			});
+    			google.maps.event.addListener(marker, "click", function() {
+    				map.panTo(this.getPosition());
+    				infoWindow.open(map, this);
+    				infoWindow.setContent(
+    					"<p>" + item.cameralabel + "</p><img src=\"" + item.imageurl.url + "\" alt=\"The position you clicked\" />"
+    				);
+    			})
+    			allMarkers.push(marker);
+    		})
+    		$("#search").bind("search keyup", function() {
+    			$.each(allMarkers, function(i, marker) {
+		    		var label = marker.cameraLabel.toLowerCase();
+		    		var query = $("#search").val().toLowerCase();
+		    		if(label.indexOf(query) != -1) {
+		    			marker.setMap(map);
+		    		} else {
+		    			marker.setMap(null);
+		    		}
+		    	});
+		    });
+    	})
+    	.fail(function (jqxhr, status, error) {
+    		var err = status + ", " + error;
+    		alert("Request Failed: " + jqxhr.responseText);
+    	});
+      }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    var infoWindow = new google.maps.InfoWindow();
+
+    var allMarkers = [];
+
+
+});
+
